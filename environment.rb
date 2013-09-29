0cdc94ff71a319f17d623f259fd2a8c924c44abd
Devin Gaffney <itsme@devingaffney.com>
2013-09-28 18:41:18 -0700

    woo

--- environment.rb
+++ environment.rb
@@ -6,6 +6,7 @@ require 'resque'
 MongoMapper.connection = Mongo::MongoClient.new("localhost", 27017, :pool_size => 25, :pool_timeout => 60)
 MongoMapper.database = "news"
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each {|file| require file }
+Dir[File.dirname(__FILE__) + '/lib/article_processors/*.rb'].each {|file| require file }
 Dir[File.dirname(__FILE__) + '/lib/crawlers/*.rb'].each {|file| require file }
 Dir[File.dirname(__FILE__) + '/lib/scorers/*.rb'].each {|file| require file }
 Dir[File.dirname(__FILE__) + '/lib/model/embedded_documents/*.rb'].each {|file| require file }
