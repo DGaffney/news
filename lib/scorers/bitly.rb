dec67d31dcd54fc6653459b191b0b3896f54cae8
Adam Jetmalani <pleasedoughnutreply@gmail.com>
2013-10-05 23:15:47 -0700

    bug free is the way to be

--- lib/scorers/bitly.rb
+++ lib/scorers/bitly.rb
@@ -1,6 +1,6 @@
 require 'bitly'
 class BitlyScorer < Scorer
   def self.score(url)
-    return JSON.parse(Cache.get(url, "bitly"))
+    Cache.get(url, "bitly")
   end
 end
--- lib/scorers/shared_count.rb
+++ lib/scorers/shared_count.rb
@@ -1,5 +1,5 @@
 class SharedCountScorer < Scorer
   def self.score(url)
-    return JSON.parse(Cache.get(url, "shared_count"))
+    Cache.get(url, "shared_count")
   end
 end
