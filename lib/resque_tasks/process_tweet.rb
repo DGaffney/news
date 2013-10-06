b0656954f9fd2c477f6614102ee10648f51ad2f0
Devin Gaffney <itsme@devingaffney.com>
2013-10-05 17:13:52 -0700

    add twitter spec

--- lib/resque_tasks/process_tweet.rb
+++ lib/resque_tasks/process_tweet.rb
@@ -2,9 +2,8 @@ class ProcessTweet
   @queue = :main
   
   def self.perform(credentials, status, statuses_count, user)
-    binding.pry
     account = Account.first(credentials: credentials)
-    Provider::Twitter::Tweet.new_from_raw(status.merge(account_id: account.id))
+    Provider::Twitter::Tweet.new_from_raw(status.merge(account_id: account.id)).save
     Resque.enqueue(AnalyzeTweets, credentials) if statuses_count == Provider::Twitter::Tweet.where(account_id: account.id).count
   end
 end
