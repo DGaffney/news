e85c4d5e9ec53c88b7c33a8db4961f5fb8979345
Devin Gaffney <itsme@devingaffney.com>
2013-10-20 16:06:33 -0700

    small things

--- lib/scorers/shared_count.rb
+++ lib/scorers/shared_count.rb
@@ -5,6 +5,8 @@ class SharedCountScorer < Scorer
   
   def self.percentile(url)
     shared_count_data = Cache.first(resource: "shared_count", url: url)
+    SharedCountScorer.store_raw(url) if shared_count_data.nil?
+    raise "Shared Count Data was not requested at run time. Failing and retrying."
     shared_count_data = shared_count_data.content
     keys = shared_count_data.keys
     other_shared_count_scores = Cache.fields(:content).where(resource: "shared_count").limit(1000).order(:_rand).collect{|c| c.content rescue nil}.compact
