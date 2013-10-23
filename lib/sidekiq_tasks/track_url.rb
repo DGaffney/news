2fc6b0a481b6787e5baa0c0cfb1186ee0e8367ad
Devin Gaffney <itsme@devingaffney.com>
2013-10-22 21:52:13 -0700

    add logos

--- lib/sidekiq_tasks/track_url.rb
+++ lib/sidekiq_tasks/track_url.rb
@@ -3,6 +3,6 @@ class TrackURL
   
   def perform(article_id, ego_id)
     ego_id = nil if ego_id == "none"
-    ArticleHit.first(:article_id => article_id, :ego_id => ego_id).increment
+    ArticleHit.first_or_create(:article_id => article_id, :ego_id => ego_id).increment
   end
 end
